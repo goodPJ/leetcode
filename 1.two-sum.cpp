53739c8aafef78d201a2f3bb28402a83b272343c
goodPJ <goodpj97@gmail.com>
2026-01-05 21:11:27 -0500

    1.two-sum.cpp

---
+++ 1.two-sum.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+// Hash map solution
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> num_map; // value -> index
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (num_map.find(complement) != num_map.end()) {
+                return {num_map[complement], i};
+            }
+            num_map[nums[i]] = i;
+        }
+        return {}; // 如果没有找到（LeetCode假设总有解）
+    };
+};
+// @lc code=end
+
+// basic solution:brute force
+// vector<int> twoSum(vector<int>& nums, int target) {
+//         int i = 0;
+//         bool flag = false;
+//         vector<int> result;
+//         for (i = 0; i < nums.size(); i++) {
+//             for (int j = i + 1; j < nums.size(); j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     result.push_back(i);
+//                     result.push_back(j);
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//             if (flag)
+//                 break;
+//         }
+//         return result;
+//     }
